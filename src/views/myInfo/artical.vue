dba4cc6ec0a5464639c85063ddd5cd69ca98b752
va-vae <2278024346@qq.com>
2021-03-15 17:41:08 +0800

    菜单、拦截、路由

---
+++ src/views/myInfo/artical.vue
@@ -0,0 +1,13 @@
+<template>
+<div>我的文章</div>
+</template>
+
+<script>
+  export default {
+    name: "artical"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/myInfo/photo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>我的相册</div>
+</template>
+
+<script>
+  import Cookies from 'js-cookie'
+  export default {
+    name: "photo",
+    beforeRouteEnter(to, from, next) {
+      if (Cookies.get('token')) {
+        next()
+      } else {
+        this.$router.replace('/login')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
